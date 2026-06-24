ebd826ad86c7387f202efeb55d9730526c84281a
dheeraj-ippakayal <80457933+dheeraj-ippakayal@users.noreply.github.com>
2026-06-24 08:39:35 +0530

    Changed python1.py file added multi from github

--- python1.py
+++ python1.py
@@ -2,6 +2,12 @@
 number_one = 10
 number_two = 20
 
+# Calculate the sum using the + operator
+total_sum = number_one * number_two
+
+# Display the result
+print(f"The sum is: {total_sum}")
+
 # Calculate the multiplication using the * operator
 total_multi = number_one * number_two
 
