fcde00955976fccc1bc8e1b8d940dc6241ce553f
dheeraj-ippakayal <80457933+dheeraj-ippakayal@users.noreply.github.com>
2026-06-24 08:36:57 +0530

    Changed python1 file in github
    Updated the code to calculate multiplication instead of sum.

--- python1.py
+++ python1.py
@@ -2,8 +2,8 @@
 number_one = 10
 number_two = 20
 
-# Calculate the sum using the + operator
-total_sum = number_one + number_two
+# Calculate the multiplication using the * operator
+total_multi = number_one * number_two
 
 # Display the result
-print(f"The sum is: {total_sum}")
+print(f"The multiplication is: {total_multi}")
