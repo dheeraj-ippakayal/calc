223a97ecfb3181c3bd3146e271d2d2a4b7477302
GUPTA <GUPTA@example.com>
2026-06-24 08:28:52 +0530

    python1.py file added

---
+++ python1.py
@@ -0,0 +1,9 @@
+# Define the two numbers
+number_one = 10
+number_two = 20
+
+# Calculate the sum using the + operator
+total_sum = number_one + number_two
+
+# Display the result
+print(f"The sum is: {total_sum}")
